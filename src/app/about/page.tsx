cb40df67433d39c1ee5343dc564bb5e83713ad8c
masonrandall <mason@blueplate.ai>
2025-02-11 17:18:27 -0800

    -initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Button from "../../../components/Button";
+
+export default async function BlogPage() {
+
+    // FETCH BLOG POSTS
+
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the about page</p>
+            <Button>This is a clientside button</Button>
+        </div>
+    )
+}
